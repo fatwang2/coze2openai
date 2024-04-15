48fa79bfa06b02e03e144cd62c2cf10b104813ea
fatwang2 <wangding0798@gmail.com>
2024-04-16 01:03:54 +0800

    support coze2openai

--- README.md
+++ README.md
@@ -1,1 +1,89 @@
-# coze2openai
+## COZE2OPENAI
+**Use Coze on your favorite OpenAI client.**
+
+This project converts the Coze API to the OpenAI API format, giving you access to Coze's LLMs, knowledge base, tools, and workflows within your preferred OpenAI clients.
+
+
+## Features
+- Convert Coze API into an OpenAI API
+- Support streaming and blocking
+- Support Chatbots API on Coze
+
+## Deployment
+### Zeabur
+[![Deploy on Zeabur](https://zeabur.com/button.svg)](https://zeabur.com/templates/92RLEZ?referralCode=fatwang2)
+
+### Vercel
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https://github.com/fatwang2/Coze2openai&env=Coze_API_URL&envDescription=https://api.Coze.ai/v1)
+
+**Note:** Vercel's serverless functions have a 10-second timeout limit.
+
+
+### Local Deployment
+1. Set the environment variable in the .env file
+```bash
+BOT_ID=
+```
+
+2. Install dependencies 
+```bash
+npm install
+```
+
+3. Run the project
+```bash
+npm start
+```
+
+## Usage
+```JavaScript
+const response = await fetch('http://localhost:3000/v1/chat/completions', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer YOUR_COZE_API_KEY',
+  },
+  body: JSON.stringify({
+    model: 'Coze',
+    messages: [
+      { role: 'system', content: 'You are a helpful assistant.' },
+      { role: 'user', content: 'Hello, how are you?' },
+    ],
+  }),
+});
+
+const data = await response.json();
+console.log(data);
+```
+## Environment Variable
+This project provides some additional configuration items set with environment variables:
+
+| Environment Variable | Required | Description                                                                                                                                                               | Example                                                                                                              |
+| -------------------- | -------- | ------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | -------------------------------------------------------------------------------------------------------------------- |
+| `BOT_ID`     | Yes      | The ID of the bot. Obtain it from the Develop page URL of your bot in Coze. The number after the bot parameter is the bot ID.| `73428668*****`|
+
+## Roadmap
+**Coming Soon**
+*   Image support
+*   Audio-to-text
+*   Text-to-audio
+*   Docker support
+*   Workflow Bot
+*   Variables support
+
+**Available Now**
+*   Continuous dialogue
+*   Zeabur & Vercel deployment
+*   Streaming & Blocking
+*   Plugins on Coze
+
+## Contact
+Feel free to reach out for any questions or feedback
+
+[X](https://sum4all.site/twitter)\
+[telegram](https://sum4all.site/telegram)
+
+<a href="https://www.buymeacoffee.com/fatwang2" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;" ></a>
+
+## License
+This project is licensed under the MIT License.
---
+++ app.js
@@ -0,0 +1,234 @@
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import fetch from "node-fetch";
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+const bot_id = process.env.BOT_ID || "";
+
+app.get("/", (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <title>COZE2OPENAI</title>
+      </head>
+      <body>
+        <h1>Coze2OpenAI</h1>
+        <p>Congratulations! Your project has been successfully deployed.</p>
+      </body>
+    </html>
+  `);
+});
+
+app.post("/v1/chat/completions", async (req, res) => {
+  const authHeader =
+    req.headers["authorization"] || req.headers["Authorization"];
+  if (!authHeader) {
+    return res.status(401).json({
+      code: 401,
+      errmsg: "Unauthorized.",
+    });
+  } else {
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({
+        code: 401,
+        errmsg: "Unauthorized.",
+      });
+    }
+  }
+  try {
+    const data = req.body;
+    const messages = data.messages;
+    const chatHistory = [];
+    for (let i = 0; i < messages.length - 1; i++) {
+      const message = messages[i];
+      const role = message.role;
+      const content = message.content;
+      
+      chatHistory.push({
+        role: role,
+        content: content,
+        content_type: "text"
+      });
+    }
+
+    const lastMessage = messages[messages.length - 1];
+    const queryString = lastMessage.content;
+    const stream = data.stream !== undefined ? data.stream : false;
+    let requestBody;
+    requestBody = {
+      query: queryString,
+      stream: stream,
+      conversation_id: "",
+      user: "apiuser",
+      bot_id: bot_id,
+      chat_history: chatHistory
+    };
+
+    const resp = await fetch("https://api.coze.com/open_api/v2/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${authHeader.split(" ")[1]}`,
+      },
+      body: JSON.stringify(requestBody),
+    });
+    if (stream) {
+      res.setHeader("Content-Type", "text/event-stream");
+      const stream = resp.body;
+      let buffer = "";
+
+      stream.on("data", (chunk) => {
+        buffer += chunk.toString();
+        let lines = buffer.split("\n");
+
+        for (let i = 0; i < lines.length - 1; i++) {
+          let line = lines[i].trim();
+
+          if (!line.startsWith("data:")) continue;
+          line = line.slice(5).trim();
+          let chunkObj;
+          try {
+            if (line.startsWith("{")) {
+              chunkObj = JSON.parse(line);
+            } else {
+              continue;
+            }
+          } catch (error) {
+            console.error("Error parsing chunk:", error);
+            continue;
+          }
+          if (chunkObj.event === "message") {
+            if (
+              chunkObj.message.role === "assistant" &&
+              chunkObj.message.type === "answer"
+            ) {
+              let chunkContent = chunkObj.message.content;
+
+              if (chunkContent !== "") {
+                const chunkId = `chatcmpl-${Date.now()}`;
+                const chunkCreated = Math.floor(Date.now() / 1000);
+                res.write(
+                  "data: " +
+                    JSON.stringify({
+                      id: chunkId,
+                      object: "chat.completion.chunk",
+                      created: chunkCreated,
+                      model: data.model,
+                      choices: [
+                        {
+                          index: 0,
+                          delta: {
+                            content: chunkContent,
+                          },
+                          finish_reason: null,
+                        },
+                      ],
+                    }) +
+                    "\n\n"
+                );
+              }
+            }
+          } else if (chunkObj.event === "done") {
+            const chunkId = `chatcmpl-${Date.now()}`;
+            const chunkCreated = Math.floor(Date.now() / 1000);
+            res.write(
+              "data: " +
+                JSON.stringify({
+                  id: chunkId,
+                  object: "chat.completion.chunk",
+                  created: chunkCreated,
+                  model: data.model,
+                  choices: [
+                    {
+                      index: 0,
+                      delta: {},
+                      finish_reason: "stop",
+                    },
+                  ],
+                }) +
+                "\n\n"
+            );
+            res.write("data: [DONE]\n\n");
+            res.end();
+          } else if (chunkObj.event === "ping") {
+          } else if (chunkObj.event === "error") {
+            console.error(`Error: ${chunkObj.code}, ${chunkObj.message}`);
+            res
+              .status(500)
+              .write(
+                `data: ${JSON.stringify({ error: chunkObj.message })}\n\n`
+              );
+            res.write("data: [DONE]\n\n");
+            res.end();
+          }
+        }
+
+        buffer = lines[lines.length - 1];
+      });
+    } else {
+      resp
+        .json()
+        .then((data) => {
+          if (data.code === 0 && data.msg === "success") {
+            const messages = data.messages;
+            const answerMessage = messages.find(
+              (message) =>
+                message.role === "assistant" && message.type === "answer"
+            );
+
+            if (answerMessage) {
+              const result = answerMessage.content.trim();
+              const usageData = {
+                prompt_tokens: 100,
+                completion_tokens: 10,
+                total_tokens: 110,
+              };
+              const chunkId = `chatcmpl-${Date.now()}`;
+              const chunkCreated = Math.floor(Date.now() / 1000);
+
+              const formattedResponse = {
+                id: chunkId,
+                object: "chat.completion",
+                created: chunkCreated,
+                model: req.body.model,
+                choices: [
+                  {
+                    index: 0,
+                    message: {
+                      role: "assistant",
+                      content: result,
+                    },
+                    logprobs: null,
+                    finish_reason: "stop",
+                  },
+                ],
+                usage: usageData,
+                system_fingerprint: "fp_2f57f81c11",
+              };
+              const jsonResponse = JSON.stringify(formattedResponse, null, 2);
+              res.set("Content-Type", "application/json");
+              res.send(jsonResponse);
+            } else {
+              res.status(500).json({ error: "No answer message found." });
+            }
+          } else {
+            res
+              .status(500)
+              .json({ error: "Unexpected response from Coze API." });
+          }
+        })
+        .catch((error) => {
+          console.error("Error parsing JSON:", error);
+          res.status(500).json({ error: "Error parsing JSON response." });
+        });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+});
+
+app.listen(process.env.PORT || 3000);
